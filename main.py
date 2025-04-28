966eca54f6134a40a369045eda3bc5cccf9e1670
vadimkurka <38429918+vadimkurka@users.noreply.github.com>
2025-04-27 22:58:37 -0600

    main.py

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def index():
     return 'LightMind Helper Bot is running!'
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
